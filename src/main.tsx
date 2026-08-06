2d908b9aa90c640079a73a4aa69b53a685f66036
Wilson <wilsonimport@gmail.com>
2026-08-06 11:54:13 -0300

    SEO: add route prerendering with react-snap and hydration

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import App from '@/App'
 import '@/styles/globals.css'
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot, hydrateRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -11,12 +11,18 @@ if (!rootElement) {
   throw new Error('Root element not found')
 }
 
-createRoot(rootElement).render(
+const app = (
   <StrictMode>
     <HelmetProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </HelmetProvider>
-  </StrictMode>,
+  </StrictMode>
 )
+
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(rootElement, app)
+} else {
+  createRoot(rootElement).render(app)
+}
